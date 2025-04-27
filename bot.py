38cd70c3d0220cf8d8a51b48a666cb7304c5510b
amirbex <amirbex@gmail.com>
2025-04-27 23:50:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import google.generativeai as genai
 # --- تنظیمات اولیه ---
 nest_asyncio.apply()
 TELEGRAM_TOKEN = '7843819663:AAED6HyqaLKdANVHq3kvqvYua9koAJp14Ts'
-OPENAI_API_KEY = 'AIzaSyC8VK_y5ESVLZNXI80wy7KBJ5_IxEoxh7E'
+GOOGLE_API_KEY = 'AIzaSyC8VK_y5ESVLZNXI80wy7KBJ5_IxEoxh7E'
 FILE_PATH = 'user_data.json'
 
 genai.configure(api_key=GOOGLE_API_KEY)
