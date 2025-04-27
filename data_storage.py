471b36155baaa2c0bde56a782470ca9c47207a0e
amirbex <amirbex@gmail.com>
2025-04-27 18:48:30 +0330

    Update data_storage.py

--- data_storage.py
+++ data_storage.py
@@ -1,31 +1,26 @@
-import csv
 import os
+import json
 
-# فایل CSV که داده‌ها در آن ذخیره می‌شود
-FILE_PATH = 'user_data.csv'
+FILE_PATH = 'user_data.json'
 
 def initialize_data_storage():
-    """بررسی و ایجاد فایل CSV اگر وجود نداشته باشد."""
     if not os.path.exists(FILE_PATH):
-        with open(FILE_PATH, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow(['نام کاربر', 'شماره تلفن', 'نوشیدنی انتخابی', 'رسپی'])
+        with open(FILE_PATH, 'w') as f:
+            json.dump([], f)
 
 def store_user_data(user_name: str, user_phone: str, selected_drink: str, recipe: dict):
-    """ذخیره اطلاعات کاربر و نوشیدنی انتخابی در فایل CSV."""
-    recipe_text = "\n".join([f"{ingredient}: {quantity}" for ingredient, quantity in recipe.items()])
-    new_data = [user_name, user_phone, selected_drink, recipe_text]
-
-    with open(FILE_PATH, mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(new_data)
+    new_data = {
+        'نام کاربر': user_name,
+        'شماره تلفن': user_phone,
+        'نوشیدنی انتخابی': selected_drink,
+        'رسپی': recipe
+    }
+    with open(FILE_PATH, 'r') as f:
+        data = json.load(f)
+    data.append(new_data)
+    with open(FILE_PATH, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 def get_all_user_data():
-    """خواندن تمام داده‌های ذخیره‌شده از فایل CSV."""
-    data = []
-    if os.path.exists(FILE_PATH):
-        with open(FILE_PATH, mode='r', newline='', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                data.append(row)
-    return data
+    with open(FILE_PATH, 'r') as f:
+        return json.load(f)
